48e2707d8824f956447dea9f4198c60deaba232c
shippinglifetools <shippinglifetools@gmail.com>
2025-12-27 11:09:23 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const cacheName = "seafarer-cache-v1";
+const filesToCache = [
+  "index.html",
+  "style.css"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
